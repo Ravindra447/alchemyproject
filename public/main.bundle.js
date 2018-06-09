ce0cf9c273061bbfc103be0302ba88e031bad64f
Ravindra447 <rr.ch447@gmail.com>
2018-06-09 16:46:57 +0530

    booking added

--- public/main.bundle.js
+++ public/main.bundle.js
@@ -1892,13 +1892,13 @@ var AuthService = /** @class */ (function () {
     AuthService.prototype.registerUser = function (user) {
         var headers = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["Headers"]();
         headers.append('Content-Type', 'application/json');
-        return this.http.post('http://localhost:5000/api/users/register', user, { headers: headers })
+        return this.http.post('/api/users/register', user, { headers: headers })
             .map(function (res) { return res.json(); });
     };
     AuthService.prototype.authenticateUser = function (user) {
         var headers = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["Headers"]();
         headers.append('Content-Type', 'application/json');
-        return this.http.post('http://localhost:5000/api/users/login', user, { headers: headers })
+        return this.http.post('/api/users/login', user, { headers: headers })
             .map(function (res) { return res.json(); });
     };
     AuthService.prototype.getCurrent = function () {
@@ -1906,52 +1906,52 @@ var AuthService = /** @class */ (function () {
         this.loadToken();
         headers.append('Authorization', this.authToken);
         headers.append('Content-Type', 'application/json');
-        return this.http.get('http://localhost:5000/api/users/current', { headers: headers })
+        return this.http.get('/api/users/current', { headers: headers })
             .map(function (res) { return res.json(); });
     };
     AuthService.prototype.getPrevBookingData = function () {
-        return this.http.get('http://localhost:5000/api/users/prevBookingData')
+        return this.http.get('/api/users/prevBookingData')
             .map(function (res) { return res.json(); });
     };
     AuthService.prototype.updatePrevBookingData = function (status) {
         // console.log(status)
         var headers = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["Headers"]();
         headers.append('Content-Type', 'application/json');
-        return this.http.post('http://localhost:5000/api/users/updatePrevBooking', status, { headers: headers })
+        return this.http.post('/api/users/updatePrevBooking', status, { headers: headers })
             .map(function (res) { return res.json(); });
     };
     AuthService.prototype.updateBooking = function (status) {
         // console.log(status)
         var headers = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["Headers"]();
         headers.append('Content-Type', 'application/json');
-        return this.http.post('http://localhost:5000/api/users/updateBooking', status, { headers: headers })
+        return this.http.post('/api/users/updateBooking', status, { headers: headers })
             .map(function (res) { return res.json(); });
     };
     AuthService.prototype.InsertConfigurations = function (config) {
         // console.log(status)
         var headers = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["Headers"]();
         headers.append('Content-Type', 'application/json');
-        return this.http.post('http://localhost:5000/api/users/configurations', config, { headers: headers })
+        return this.http.post('/api/users/configurations', config, { headers: headers })
             .map(function (res) { return res.json(); });
     };
     AuthService.prototype.getConfigurations = function () {
-        return this.http.get('http://localhost:5000/api/users/getConfigurations')
+        return this.http.get('/api/users/getConfigurations')
             .map(function (res) { return res.json(); });
     };
     AuthService.prototype.getAvailableRooms = function () {
-        return this.http.get('http://localhost:5000/api/users/getAvailableRooms')
+        return this.http.get('/api/users/getAvailableRooms')
             .map(function (res) { return res.json(); });
     };
     AuthService.prototype.roomResource = function (rooms) {
         var headers = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["Headers"]();
         headers.append('Content-Type', 'application/json');
-        return this.http.post('http://localhost:5000/api/users/roomResource', rooms, { headers: headers })
+        return this.http.post('/api/users/roomResource', rooms, { headers: headers })
             .map(function (res) { return res.json(); });
     };
     AuthService.prototype.bookingRooms = function (user) {
         var headers = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["Headers"]();
         headers.append('Content-Type', 'application/json');
-        return this.http.post('http://localhost:5000/api/users/prevBooking', user, { headers: headers })
+        return this.http.post('/api/users/prevBooking', user, { headers: headers })
             .map(function (res) { return res.json(); });
     };
     AuthService.prototype.loadToken = function () {
